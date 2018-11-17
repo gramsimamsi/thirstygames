40f4a96749a51d3ab4c1ceda5dda0de2102b04b5
sWIchprit <christian.pritzl@outlook.de>
2018-11-17 16:42:33 +0100

    new router functions

---
+++ backend/routes/beverage.js
@@ -0,0 +1,17 @@
+let express = require('express');
+let router = express.Router();
+let beverageController = require("../controllers/beverageController");
+
+/*
+    ALL BEVERAGES
+*/
+router.get('/', beverageController.all_beverages_get);
+router.delete('/', beverageController.all_beverages_delete);
+
+/*
+    SINGLE BEVERAGE
+ */
+router.get('/:id', beverageController.single_beverage_get);
+router.post('/:id', beverageController.single_beverage_post);
+router.put('/:id', beverageController.single_beverage_put);
+router.delete('/:id', beverageController.single_beverage_delete);
