6bc1454e7628c463749407e02399deefeba34d68
sWIchprit <christian.pritzl@outlook.de>
2018-12-12 13:14:34 +0100

    es geht weiter :D

--- backend/utilities/LoginHandler.js
+++ backend/utilities/LoginHandler.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
 let userModel = require('../models/userModel');
 
 let jwt = require('jsonwebtoken');
 let config = require('../jwtConfig');
-let middleware = require('../middleware');
 
 const TOKEN_EXPIRATION_TIME =  '1h';
 
@@ -20,12 +17,12 @@ class LoginHandler
         console.log(username);
         console.log(password);
         //get User from Database
-        userModel.find({user_name: username}).exec(function(err, user)
+        userModel.findOne({user_name: username}).exec(function(err, user)
         {
             if (err)
             {
                 //throw err;
-                res.send(401).json({
+                res.sendStatus(401).json({
                     success: false,
                     message: 'AUTHENTICATION FAILED -> WRONG USERNAME OR PASSWORD'
                 });
@@ -43,12 +40,15 @@ class LoginHandler
                         });
                     }
                     //generate token
-                    let token = jwt.sign({username: username}, config.secret, {expiresIn: TOKEN_EXPIRATION_TIME});
-                    res.status(200).json({
-                        success: true,
-                        message: 'AUTHENTICATION SUCCESSFULL',
-                        token: token
-                    });
+                    if(isMatch)
+                    {
+                        let token = jwt.sign({username: username}, config.secret, {expiresIn: TOKEN_EXPIRATION_TIME});
+                        res.sendStatus(200).json({
+                            success: true,
+                            message: 'AUTHENTICATION SUCCESSFULL',
+                            token: token
+                        });
+                    }
                 });
             }
             else
