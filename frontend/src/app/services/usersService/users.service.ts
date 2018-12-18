7c9c814a7b003e7ba371d17cb9ee13b8dd7437a3
sWIchprit <christian.pritzl@outlook.de>
2018-12-18 11:28:00 +0100

    added list of users to /admin/users view

--- frontend/src/app/services/usersService/users.service.ts
+++ frontend/src/app/services/usersService/users.service.ts
@@ -3,6 +3,7 @@ import  {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {catchError} from "rxjs/operators";
 import {HttpClientHelper} from "../../Utilites/HttpClientHelper";
+import {User} from "../../users/User";
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +12,15 @@ export class UsersService {
 
   constructor(
     private http: HttpClient,
-  ) { }
+  ) {
+  }
 
-  private usersGet  =  'users'; /*'http://127.0.0.1:3000/login';*/
+  private usersGet = 'users'; /*'http://127.0.0.1:3000/login';*/
 
-  getAllUsers(): Observable<any>
+  getAllUsers(): Observable<User[]>
   {
-    return this.http.get(this.usersGet, HttpClientHelper.httpOptionsApplicationJSON)
-      .pipe(catchError(HttpClientHelper.handleError('GET_ALL_USERS_ERROR')));
+    return this.http.get<User[]>(this.usersGet, HttpClientHelper.httpOptionsApplicationJSON)
+      .pipe(catchError(HttpClientHelper.handleError('GET_ALL_USERS_ERROR', [])));
   }
 }
+
--- frontend/src/app/users/users.component.ts
+++ frontend/src/app/users/users.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UsersService} from "../services/usersService/users.service";
+import {Observable} from "rxjs";
+import {User} from "./User";
 
 @Component({
   selector: 'app-users',
@@ -10,10 +12,16 @@ export class UsersComponent implements OnInit {
 
   constructor(private userService: UsersService) { }
 
+  users: User[];
+
   showAllUsers(): void
   {
     this.userService.getAllUsers().subscribe(
-      body => console.log(body)
+      users => {
+        this.users = users;
+
+      },
+      error => console.log("ERROR GETTING USERS -> " + error.toString())
     );
   }
 
