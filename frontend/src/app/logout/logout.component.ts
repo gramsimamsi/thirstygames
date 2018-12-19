418afa9d6700c2d7278380ee0f5c60a6116f17cd
chriss <chriss@chriss-desktop-ubuntu.fritz.box>
2018-12-19 22:51:56 +0100

    added some minor css fixes and a html for logout page

--- frontend/src/app/logout/logout.component.ts
+++ frontend/src/app/logout/logout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-logout',
@@ -10,6 +11,11 @@ export class LogoutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    //logout user -> remove all entries in localStorage? or only accessToken
+    localStorage.removeItem('accessToken');
   }
 
 }
+
+
+// ToDo -> deny access to pages after login, currently user can only not do api-calls
