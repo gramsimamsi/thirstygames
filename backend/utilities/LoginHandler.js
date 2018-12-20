5c100e4b80bcfb4f39aae0e38a7927e52b21417b
chriss <chriss@chriss-desktop-ubuntu.fritz.box>
2018-12-20 23:14:15 +0100

    solved problem of empty password on server

--- backend/utilities/LoginHandler.js
+++ backend/utilities/LoginHandler.js
@@ -12,9 +12,6 @@ class LoginHandler
         let username = req.body.user_name;
         let password = req.body.user_password;
 
-        console.log(username);
-        console.log(password);
-
         if(username && password)
         {
             userModel.findOne({user_name: username}).exec(function(err, user)
@@ -30,60 +27,68 @@ class LoginHandler
                     return;
                 }
 
-                user.comparePassword(password, user.user_password).then(function (isMatch)
+                if(user)
                 {
-                    if(isMatch)
+                    user.comparePassword(password, user.user_password).then(function (isMatch)
                     {
-                        let token = jwt.sign({username: username}, config.secret, {expiresIn: config.tokenExpirationTime});
-                        let refreshToken = jwt.sign({username: username}, config.refreshSecret, {expiresIn: config.refreshTokenExpirationTime});
-
-                        // save refresh-token in DB with every login
-                        userModel.findOneAndUpdate({user_name: username}, {user_refresh_token: refreshToken}, {upsert:true}, function(err, doc)
+                        if(isMatch)
                         {
-                            if(err)
+                            let token = jwt.sign({username: username}, config.secret, {expiresIn: config.tokenExpirationTime});
+                            let refreshToken = jwt.sign({username: username}, config.refreshSecret, {expiresIn: config.refreshTokenExpirationTime});
+
+                            // save refresh-token in DB with every login
+                            userModel.findOneAndUpdate({user_name: username}, {user_refresh_token: refreshToken}, {upsert:true}, function(err, doc)
                             {
-                                console.log("could not update " + username.toString() + " with new refreshToken");
-                                res.status(401).json({
-                                    success:false,
-                                    message: 'AUTHENTICATION FAILED -> WRONG USERNAME OR PASSWORD'
-                                });
-                            }
-                            else
-                            {
-                                //console.log("inserted new refresh-token -> user is logged in");
-                                res.status(200).json({
-                                    success: true,
-                                    message: 'AUTHENTICATION SUCCESSFULL',
-                                    token: token,
-                                    refreshToken: refreshToken
-                                });
-                            }
-                        })
-                    }
-                    else
+                                if(err)
+                                {
+                                    console.log("could not update " + username.toString() + " with new refreshToken");
+                                    res.status(401).json({
+                                        success:false,
+                                        message: 'AUTHENTICATION FAILED -> WRONG USERNAME OR PASSWORD'
+                                    });
+                                }
+                                else
+                                {
+                                    //console.log("inserted new refresh-token -> user is logged in");
+                                    res.status(200).json({
+                                        success: true,
+                                        message: 'AUTHENTICATION SUCCESSFULL',
+                                        token: token,
+                                        refreshToken: refreshToken
+                                    });
+                                }
+                            })
+                        }
+                        else
+                        {
+                            //no error, but nothing found
+                            res.status(401).json({
+                                success:false,
+                                message: 'AUTHENTICATION FAILED -> WRONG USERNAME OR PASSWORD'
+                            });
+                        }
+                    }).catch(function (err)
                     {
-                        //no error, but nothing found
-                        res.status(401).json({
-                            success:false,
-                            message: 'AUTHENTICATION FAILED -> WRONG USERNAME OR PASSWORD'
-                        });
-                    }
-                }).catch(function (err)
-                {
-                    if (err)
-                    {
-                        console.log("catch -> " + err.toString());
-                        res.status(401).json({
-                            success: false,
-                            message: 'AUTHENTICATION FAILED -> WRONG USERNAME OR PASSWORD'
-                        });
-                    }
-                })
+                        if (err)
+                        {
+                            console.log("catch -> " + err.toString());
+                            res.status(401).json({
+                                success: false,
+                                message: 'AUTHENTICATION FAILED -> WRONG USERNAME OR PASSWORD'
+                            });
+                        }
+                    })
+                }
+                else {
+                    res.status(401).send();
+                    console.log("No password provided");
+                }
+
             });
         }
         else
         {
-            res.status(400);
+            res.status(401);
             console.log("Username ->" + username + " Passwort ->" + password);
         }
     }
