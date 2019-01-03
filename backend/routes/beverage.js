69f5b02dce878ab71b59154ae748ffa294fcdce5
Thomas Randl <thomas@localhost.localdomain>
2019-01-03 18:42:12 +0100

    Beverage has now only get all

--- backend/routes/beverage.js
+++ backend/routes/beverage.js
@@ -8,14 +8,5 @@ let beverageController = require("../controllers/beverageController");
 */
 
 router.get('/', middleware.checkToken, middleware.isBarkeeper,beverageController.all_beverages_get);
-router.delete('/', middleware.checkToken, middleware.isAdmin, beverageController.all_beverages_delete);
-
-/*
-    SINGLE BEVERAGE
- */
-router.get('/:beverage_id', middleware.checkToken, middleware.isAdmin, beverageController.single_beverage_get);
-router.delete('/:beverage_id', middleware.checkToken, middleware.isAdmin, beverageController.single_beverage_delete);
-router.post('/', middleware.checkToken, middleware.isAdmin, beverageController.single_beverage_post);
-router.put('/:beverage_id', middleware.checkToken, middleware.isAdmin, beverageController.single_beverage_put);
 
 module.exports = router;
