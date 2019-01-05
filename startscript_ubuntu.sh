9ff8f13759f902d2de51c7f39d3061d6d9bf881a
Thomas Randl <thomas@localhost.localdomain>
2019-01-05 15:26:24 +0100

    anpassungen startscript ubunt u

--- startscript_ubuntu.sh
+++ startscript_ubuntu.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-sudo apt-get install docker-ce=3:18.09.0~ce-3~ubuntu
+sudo apt install docker-ce=3:18.09.0~ce-3~ubuntu
 # shows newest docker version
 # apt-cache madison docker-ce
+sudo service docker start
 sudo docker-compose up
