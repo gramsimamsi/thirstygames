6880322bb9c28fd2c48fbd193838118283a0b9c1
sINFlugram <lukas.grams@stud.fh-rosenheim.de>
2019-01-05 19:03:19 +0100

    updated models

--- frontend/src/app/models/Team.ts
+++ frontend/src/app/models/Team.ts
@@ -1,6 +1,5 @@
 export class Team {
   team_name: string;
-  team_member_count: number;
   team_alc_count: number;
   _id: string;
 }
--- frontend/src/app/models/User.ts
+++ frontend/src/app/models/User.ts
@@ -1,8 +1,6 @@
 /*abstraction of User*/
 export class User {
   user_name: string;
-  user_password: string;
   user_role: number;
-  user_refresh_token: string;
   _id: string;
 }
