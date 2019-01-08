c2c13b2f04e17af4febcd1c525dd84660a0c0c84
chriss <chriss@chriss-desktop-ubuntu.fritz.box>
2019-01-08 19:54:15 +0100

    user is now logged out and redirected to login if token is expired

--- frontend/src/app/Utilites/accessToken-interceptor.ts
+++ frontend/src/app/Utilites/accessToken-interceptor.ts
@@ -3,12 +3,16 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor, HttpResponse, HttpErrorResponse
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
 import { AuthService } from '../services/authService/auth.service';
 import { Observable } from 'rxjs/Observable';
-import "rxjs-compat/add/operator/do";
-import {Router} from "@angular/router";
+import 'rxjs-compat/add/operator/do';
+import 'rxjs/add/operator/catch';
+
+import {Router} from '@angular/router';
+import {throwError} from 'rxjs';
 
 @Injectable()
 export class AccessTokenInterceptor implements HttpInterceptor {
@@ -25,28 +29,15 @@ export class AccessTokenInterceptor implements HttpInterceptor {
       }
     });
 
-    return next.handle(request);
-
-    /*testimplementation for token expiration check */
-
-    /*
-    return next.handle(request).do( (event: HttpEvent<any>) => {
-
-      if(event instanceof HttpResponse)
-      {
-        //nothing to do everythink fine
-      }
-    }, (err: any) =>
-      {
-        if(err instanceof HttpErrorResponse)
-        {
-          if(err.status === 401)
-          {
-            //redirect to login page -> token expired -> redirect is very nice :D
-            this.router.navigateByUrl('token');
+    return next.handle(request)
+      .catch(error => {
+        console.log('Auth-Token is expired');
+        if (error instanceof HttpErrorResponse) {
+          if (error.status === 401) {
+            this.router.navigateByUrl('/');
           }
-        }
-      })
-  }*/
+        } return throwError(error);
+      }) as any;
   }
 }
+
