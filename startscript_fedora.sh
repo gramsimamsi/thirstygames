39bb90dab928bd8a7d7e2c9b5998095ec37a8fab
Thomas Randl <thomas@localhost.localdomain>
2019-01-09 08:37:03 +0100

    Startscript funktions working

--- startscript_fedora.sh
+++ startscript_fedora.sh
@@ -7,9 +7,9 @@ sudo dnf -y install docker-ce-3:18.09.0-3.fc28
 sudo systemctl start docker
 #sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 #sudo chmod +x /usr/local/bin/docker-compose
-sudo docker-compose up&
-while [[ -z `sudo docker ps -q --no-trunc | grep $( sudo docker-compose ps -q --filter "name=thirstygameswt_database_1" )` ]]; do sleep 1; done
-sudo docker exec thirstygameswt_database_1 bash -c "
+sudo docker-compose up -d
+sleep 2
+sudo docker exec thirstygames_wt_database_1 bash -c "
         mongo thirstyGames --eval 'db.users.save({ \"user_name\" : \"admin\", \"user_password\" : \"\$2b\$10\$S0qzD5J2WE.POZGzNH2Kou8MU/jwjhu0tRMM8rLebcuK1Gn5YRaVy\", \"user_role\" : 0 })' &&
         mongo thirstyGames --eval 'db.beverages.save({ \"beverage_name\" : \"Bier\", \"beverage_alc\" : 5.0 })' &&
         mongo thirstyGames --eval 'db.beverages.save({ \"beverage_name\" : \"Schnaps\", \"beverage_alc\" : 40.0 })' &&
