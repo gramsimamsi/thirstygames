73290bd6d8c27cdbd31999d2777659720a1f2213
sINFlugram <lukas.grams@stud.fh-rosenheim.de>
2019-01-12 20:02:38 +0100

    added todo to backend

--- backend/controllers/teamController.js
+++ backend/controllers/teamController.js
@@ -39,6 +39,7 @@ module.exports = (webSocketServer) => {
       });
     },
     /* update a team*/
+    // TODO : this returns an array containing a single team as response. return only the team and TELL LUKAS, please :)
     single_team_put(req, res, next) {
       if (!req.body.team_alc_count || !req.body.team_name) {
         return res.status(400).send();
