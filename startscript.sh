b2c3638fc5407de2de0451e9a1863dbe62e05317
Thomas Randl <thomas@localhost.localdomain>
2019-01-15 14:06:26 +0100

    One startscript for all linux distributions

--- startscript.sh
+++ startscript.sh
@@ -17,6 +17,51 @@ else
     databaseFilled=0
 fi
 
+# Determine OS platform
+UNAME=$(uname | tr "[:upper:]" "[:lower:]")
+# If Linux, try to determine specific distribution
+if [ "$UNAME" == "linux" ]; then
+    # If available, use LSB to identify distribution
+    if [ -f /etc/lsb-release -o -d /etc/lsb-release.d ]; then
+        export DISTRO=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
+    # Otherwise, use release info file
+    else
+        export DISTRO=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1)
+    fi
+fi
+# For everything else (or if above failed), just use generic identifier
+[ "$DISTRO" == "" ] && export DISTRO=$UNAME
+unset UNAME
+
+if [ $DISTRO = "Fedora" ]
+then
+    echo "Do you wish to install docker? [1/2]"
+    select yn in "Yes" "No"; do
+        case ${yn} in
+            Yes ) sudo dnf -y install docker-ce-3:18.09.0-3.fc28;
+                  # shows newest version
+                  sudo dnf list docker-ce  --showduplicates | sort -r;
+                  break;;
+            No ) break;;
+        esac
+    done
+    echo "Do you wish to start docker? [1/2]"
+    select yn in "Yes" "No"; do
+        case ${yn} in
+            Yes ) sudo systemctl start docker; break;;
+            No ) break;;
+        esac
+    done
+    echo "Do you wish to install docker-compose? [1/2]"
+    select yn in "Yes" "No"; do
+        case ${yn} in
+            Yes ) sudo sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
+                sudo chmod +x /usr/local/bin/docker-compose; break;;
+            No ) break;;
+        esac
+    done
+fi
+
 sudo docker-compose up -d
 
 sleep 1
@@ -96,5 +141,3 @@ then
     done
 fi
 echo "System up and ready"
-
-
